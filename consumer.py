fde5632028cbdd175bd7292070cb96470c2c83a5
Karli White <karli@zipflow.com>
2025-10-24 16:44:13 -0600

    Finished command line arguments that are necessary for assignment

--- consumer.py
+++ consumer.py
@@ -1,13 +1,27 @@
-import argparse
+import argparse, textwrap
 
 # parser = argparse.ArgumentParser()
 # parser.parse_args()
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Consumer Application for Widget Requests")
+    parser = argparse.ArgumentParser(
+        description="Command-line arguments (both short and long styles):",
+        epilog=textwrap.dedent('''\
+            Examples:
+              python consumer.py -rb usu-cs5250-blue-requests -wb usu-cs5250-blue-web
+              python consumer.py --request-bucket=usu-cs5250-blue-requests --widget-bucket=usu-cs5250-blue-web
+              python consumer.py -rb my-requests -dwt widgets
+        '''),
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
 
-    parser.add_argument("--request-bucket", "-rb", type=str, required=True, metavar="BUCKET_NAME",
-                        help="Name of the bucket that contains widget requests.") # reading a single widget requests from bucket 2 in key order
+    parser.add_argument("--request-bucket", "-rb", type=str, required=True, metavar="BUCKET_NAME", help="Name of the bucket that contains widget requests.") # reading a single widget requests from bucket 2 in key order
+    
+    parser.add_argument("--widget-bucket", "-wb", type=str, required=True, metavar="BUCKET_NAME",
+                        help="Name of the S3 bucket that holds the widgets")
+
+    parser.add_argument("--dynamodb-widget-table", "-dwt", type=str, required=True, metavar="BUCKET_NAME",
+                        help="Name of the DynamoDB table that holds widgets")  
     
     return parser.parse_args()
 
